cedf1a20d0ce18d192d377584fc15c5ee8a9419c
Nick Vandal <vandalusna@gmail.com>
2012-03-23 17:48:29 -0400

    Added commandline option -T
    Added ability to specifiy which intervals to calculate gpa for at
    the command line.

--- scripts/parse_gpa.pl
+++ scripts/parse_gpa.pl
@@ -4,8 +4,9 @@ use feature 'switch';
 use POSIX;
 
 #Command line switches
-our($weighted);
-if(not defined $weighted) {$weighted = 0;}
+our($W, $T); #Weighted, interval_string
+if(not defined $W) {$W = 0;}
+if(not defined $T) {$T= 'C/CA';}
 
 my $id_num;
 my $name_string;
@@ -30,8 +31,15 @@ my %grade_mapping_std = ('A',4.0,'B',3.0,'C',2.0,'D',1.0,'F',0.0);
 my %grade_mapping_ap = ('A',5.0,'B',4.0,'C',3.0,'D',2.0,'F',0.0);
 my %semester_map = ('01','F','02','F','03','F','04','S','05','S','06','S','07','S','08','S','09','S','10','F','11','F','12','F');
 
+
+#Parse the interval list
+my @interval_list = split(/[^A|^F|^S|^C|^\d]/,$T);
+foreach(@interval_list)
+{
+	print "<$_>\n";
+}
+
 #Sample data...TODO will be passed in as cmdline argument
-my @interval_list = ('F09','F09A','S09','S09A','09','09A','F10','F10A','S10','S10A','10','10A','C','CA');
 my @ac_course_list = ("230109","361401","230110","310402","361402","370128","310401","370127","310301","360701","230108","310302","360702","230107");
 
 sub round_gpa
@@ -74,7 +82,7 @@ while (<>) {
 		when(/(^\d{6}) (.+?)  (\d.\d)  (.{3})  Gr(\d\d)  (\d\d\/\d\d\/\d\d)  (.*)/) {
 			$course_num = $1;
 			$course_name = $2;
-			$course_weight = $weighted ? $3 : 1.0; #Check to apply equal weighting or not
+			$course_weight = $W ? $3 : 1.0; #Check to apply equal weighting or not
 			$course_score = $4;
 			$course_grade = $5;
 			$course_date = $6; #MM/DD/YY date format
@@ -149,16 +157,26 @@ while (<>) {
 					foreach  $current_interval (@interval_list)
 					{
 						#Cumulative overall/academic
-						if($current_interval =~ m/C([A|O]?)/)
+						if($current_interval =~ m/^C((\d\d)*)([A|O]?)$/)
 						{
-							my $academic_interval = $1;
+							my $academic_interval = $3;
+							my @valid_years = ( $1 =~ m/../g );
+								
+							#Match valid_years or is entire available history?
+							if((not scalar(@valid_years)) or ($course_struct->{grade} ~~ @valid_years))
+							{
+								
+								#Academic or overall?	
+								if((length($academic_interval) == 0) or ($academic_interval =~"O") or ($is_academic_course and ($academic_interval =~ "A"))){
+									$ov_gpa{$current_interval} += ($course_struct->{weight} * $course_struct->{score});
+									$ov_weight_sum{$current_interval} += $course_struct->{weight};
+									
+								}
 
-							if((length($academic_interval) == 0) or ($academic_interval =~"O") or ($is_academic_course and ($academic_interval =~ "A"))){	                                                                                $ov_gpa{$current_interval} += ($course_struct->{weight} * $course_struct->{score});
-		                                            $ov_weight_sum{$current_interval} += $course_struct->{weight};
-						     	}
+							}	
 						}
 						#Parse interval format and extract semester/year
-						elsif($current_interval =~ m/([F|S]?)(\d\d)([A|O]?)/)
+						elsif($current_interval =~ m/^([F|S]?)(\d\d)([A|O]?)$/)
 						{
 							my $current_sem = $1;
 							my $current_year = $2;
@@ -176,11 +194,15 @@ while (<>) {
 										$ov_gpa{$current_interval} += ($course_struct->{weight} * $course_struct->{score});
 										$ov_weight_sum{$current_interval} += $course_struct->{weight};
 										
-										#print "ADDED $course_struct->{name} to $current_interval. gpasum = $ov_gpa{$current_interval}, weightsum = $ov_weight_sum{$current_interval}";
 									}
 								}
 
 							}
+						}
+						else
+						{
+							print "ERROR: Invalid interval specifier string! Exiting...\n";
+							exit 1;
 						}
 					}
 				}
